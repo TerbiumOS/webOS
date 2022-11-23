e8f70bf38c16ad7c82abc5ad352fdcd0a329129b
David Reed <reedswork@proton.me>
2022-11-22 21:16:01 -0500

    update usage

--- index.mjs
+++ index.mjs
@@ -1,21 +1,38 @@
-import Server from 'bare-server-node';
-import http from 'http';
-import nodeStatic from 'node-static';
+import createBareServer from "@tomphttp/bare-server-node";
+import http from "node:http";
 
+const httpServer = http.createServer();
 
-const bare =  new Server('/bare/', '');
-const serve = new nodeStatic.Server('static/');
-
-const server = http.createServer();
-
-server.on('request', (request, response) => {
-    if (bare.route_request(request, response)) return true;
-    serve.serve(request, response);
+const bareServer = createBareServer("/", {
+  logErrors: false,
+  localAddress: undefined,
+  maintainer: {
+    email: "tomphttp@sys32.dev",
+    website: "https://github.com/tomphttp/",
+  },
 });
 
-server.on('upgrade', (req, socket, head) => {
-	if(bare.route_upgrade(req, socket, head))return;
-	socket.end();
+httpServer.on("request", (req, res) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    res.writeHead(400);
+    res.end("Not found.");
+  }
 });
 
-server.listen(process.env.PORT || 6969);
+httpServer.on("upgrade", (req, socket, head) => {
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
+});
+
+httpServer.on("listening", () => {
+  console.log("HTTP server listening");
+});
+
+httpServer.listen({
+  port: process.env.PORT || 6969,
+});
