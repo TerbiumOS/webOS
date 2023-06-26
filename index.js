0e6fa322931467ea838a577ca1b90acffe3c9697
William Barnett <49733954+NovaAppsInc@users.noreply.github.com>
2023-06-26 15:00:17 -0500

    fixed bare and removed an unused file
    - removed uv.sw-handler.js
    - fixed bare and service worker issues

--- index.js
+++ index.js
@@ -1,40 +1,45 @@
 import createBareServer from "@tomphttp/bare-server-node";
 import http from "node:http";
+import { createServer } from 'node:http';
+import { fileURLToPath } from 'node:url';
 import nodeStatic from "node-static";
+import serveStatic from 'serve-static';
 
 const httpServer = http.createServer();
-const serve = new nodeStatic.Server('static/');
+const serve = serveStatic(
+	fileURLToPath(new URL('./static/', import.meta.url)),
+	{
+		fallthrough: false,
+	}
+);
 
-const bareServer = createBareServer("/", {
-  logErrors: false,
-  localAddress: undefined,
-  maintainer: {
-    email: "tomphttp@sys32.dev",
-    website: "https://github.com/tomphttp/",
-  },
+const bareServer = createBareServer('/bare/');
+
+httpServer.on('request', (req, res) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeRequest(req, res);
+	} else {
+		serve(req, res, (err) => {
+			res.writeHead(err?.statusCode || 500, {
+				'Content-Type': 'text/plain',
+			});
+			res.end(err?.stack);
+		});
+	}
 });
 
-httpServer.on("request", (req, res) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeRequest(req, res);
-  } else {
-    res.writeHead(400);
-    res.end("Not found.");
-  }
+httpServer.on('upgrade', (req, socket, head) => {
+	if (bareServer.shouldRoute(req)) {
+		bareServer.routeUpgrade(req, socket, head);
+	} else {
+		socket.end();
+	}
 });
 
-httpServer.on("upgrade", (req, socket, head) => {
-  if (bareServer.shouldRoute(req)) {
-    bareServer.routeUpgrade(req, socket, head);
-  } else {
-    socket.end();
-  }
-});
-
-httpServer.on("listening", () => {
-  console.log("HTTP server listening");
+httpServer.on('listening', () => {
+	console.log('HTTP server listening');
 });
 
 httpServer.listen({
-  port: process.env.PORT || 6969,
+	port: process.env.PORT || 6969,
 });
--- static/sw.js
+++ static/sw.js
@@ -5,10 +5,14 @@
  * Ideally, this will be registered under the scope in uv.config.js so it will not need to be modified.
  * However, if a user changes the location of uv.bundle.js/uv.config.js or sw.js is not relative to them, they will need to modify this script locally.
  */
-importScripts('uv.bundle.js');
-importScripts('uv.config.js');
-importScripts(__uv$config.sw || 'uv.sw.js');
+try {
+    importScripts('/uv/uv.bundle.js');
+    importScripts('/uv/uv.config.js');
+    importScripts(__uv$config.sw || '/uv/uv.sw.js');
 
-const sw = new UVServiceWorker();
+    const sw = new UVServiceWorker();
 
-self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+    self.addEventListener('fetch', (event) => event.respondWith(sw.fetch(event)));
+} catch (e) {
+    console.error(e);
+}
