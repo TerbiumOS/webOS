42955bc875ec9a5d4f4ee1cf5fad124073bc9601
William Barnett <49733954+NovaAppsInc@users.noreply.github.com>
2023-06-26 15:09:55 -0500

    cleaner code

--- index.js
+++ index.js
@@ -1,45 +1,42 @@
 import createBareServer from "@tomphttp/bare-server-node";
 import http from "node:http";
-import { createServer } from 'node:http';
 import { fileURLToPath } from 'node:url';
-import nodeStatic from "node-static";
 import serveStatic from 'serve-static';
 
 const httpServer = http.createServer();
 const serve = serveStatic(
-	fileURLToPath(new URL('./static/', import.meta.url)),
-	{
-		fallthrough: false,
-	}
+  fileURLToPath(new URL('./static/', import.meta.url)), {
+    fallthrough: false,
+  }
 );
 
 const bareServer = createBareServer('/bare/');
 
 httpServer.on('request', (req, res) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeRequest(req, res);
-	} else {
-		serve(req, res, (err) => {
-			res.writeHead(err?.statusCode || 500, {
-				'Content-Type': 'text/plain',
-			});
-			res.end(err?.stack);
-		});
-	}
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeRequest(req, res);
+  } else {
+    serve(req, res, (err) => {
+      res.writeHead(err?.statusCode || 500, {
+        'Content-Type': 'text/plain',
+      });
+      res.end(err?.stack);
+    });
+  }
 });
 
 httpServer.on('upgrade', (req, socket, head) => {
-	if (bareServer.shouldRoute(req)) {
-		bareServer.routeUpgrade(req, socket, head);
-	} else {
-		socket.end();
-	}
+  if (bareServer.shouldRoute(req)) {
+    bareServer.routeUpgrade(req, socket, head);
+  } else {
+    socket.end();
+  }
 });
 
 httpServer.on('listening', () => {
-	console.log('HTTP server listening');
+  console.log('HTTP server listening');
 });
 
 httpServer.listen({
-	port: process.env.PORT || 6969,
+  port: process.env.PORT || 6969,
 });
