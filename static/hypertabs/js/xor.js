a6a7d8acb929660ac4bc74b58462e5593a84a14c
John Glynn <playingallday383@gmail.com>
2023-10-16 19:28:10 -0400

    Update deps & Fix weird xor not encoding thing

--- static/hypertabs/js/xor.js
+++ static/hypertabs/js/xor.js
@@ -1,25 +1,30 @@
 // xor function in one place for easy modification
 class xor {
   static encode(str) {
+    if (!str) return str;
     return encodeURIComponent(
-      str
-        .toString()
-        .split("")
-        .map((char, ind) =>
-          ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
-        )
-        .join("")
+        str
+            .toString()
+            .split('')
+            .map((char, ind) =>
+                ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
+            )
+            .join('')
     );
-  }
-  static decode(str) {
-    if (str.charAt(str.length - 1) == "/") str = str.slice(0, -1);
-    return decodeURIComponent(str)
-      .split("")
-      .map((char, ind) =>
-        ind % 2 ? String.fromCharCode(char.charCodeAt() ^ 2) : char
-      )
-      .join("");
-  }
+}
+static decode(str) {
+    if (!str) return str;
+    let [input, ...search] = str.split('?');
+
+    return (
+        decodeURIComponent(input)
+            .split('')
+            .map((char, ind) =>
+                ind % 2 ? String.fromCharCode(char.charCodeAt(0) ^ 2) : char
+            )
+            .join('') + (search.length ? '?' + search.join('?') : '')
+    );
+}
 }
 // and setting path easily. (wait this is in the js.js file as prefix though?)
 path = "/sw/";
