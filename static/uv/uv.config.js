771efbf47443404d346ea762a646447b2abd1b7e
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-06-15 11:01:31 -0400

    lets get down to buisness

--- static/uv/uv.config.js
+++ static/uv/uv.config.js
@@ -1,6 +1,6 @@
 /*global Ultraviolet*/
 self.__uv$config = {
-    prefix: '/sw/',
+    prefix: '/uv/service/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
