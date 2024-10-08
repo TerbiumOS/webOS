3bf5ff32403821f51e32e35adb86fedb9ea82431
John <72810050+Notplayingallday383@users.noreply.github.com>
2024-10-08 08:43:17 -0400

    hotfix

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { createBareServer } from "@tomphttp/bare-server-node";
+import { createBareServer } from "@nebula-services/bare-server-node";
 import express from "express";
 import { createServer } from "node:http";
 import path from "path";
